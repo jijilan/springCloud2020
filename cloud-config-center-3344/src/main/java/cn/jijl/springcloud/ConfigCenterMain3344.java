e1836663aaedd8f989cbfd79204d75f46661d855
jijilan <1572877719@qq.com>
2020-07-07 22:53:01 +0800

    下一章服务总线

---
+++ cloud-config-center-3344/src/main/java/cn/jijl/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package cn.jijl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author jijl
+ * @Date 21:49 2020/7/7
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
